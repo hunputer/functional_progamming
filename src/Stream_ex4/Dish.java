b84abd34ef7ff93e6501e0e020195a6f2983105e
hunputer <chlwlgns3855@naver.com>
2023-03-12 00:16:22 +0900

    commit

---
+++ src/Stream_ex4/Dish.java
@@ -0,0 +1,65 @@
+package Stream_ex4;
+
+public class Dish {
+	private String name;
+	private boolean vegetarian;
+	private int calories;
+	private Type type;
+	
+	public Dish(String name, boolean vegetarian, int calories, Type type) {
+		this.name = name;
+		this.vegetarian = vegetarian;
+		this.calories = calories;
+		this.type = type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public boolean isVegetarian() {
+		return vegetarian;
+	}
+
+
+
+	public void setVegetarian(boolean vegetarian) {
+		this.vegetarian = vegetarian;
+	}
+
+
+
+	public int getCalories() {
+		return calories;
+	}
+
+
+
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+
+
+
+	public Type getType() {
+		return type;
+	}
+
+
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+
+
+	public enum Type {MEAT, FISH, OTHER};
+}
---
+++ src/Stream_ex4/Main.java
@@ -0,0 +1,45 @@
+package Stream_ex4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import stream_ex1.Dish;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Dish> menu = Arrays.asList(
+	              new Dish("pork", false, 800, Dish.Type.MEAT),
+	              new Dish("beef", false, 700, Dish.Type.MEAT),
+	              new Dish("chicken", false, 400, Dish.Type.MEAT),
+	              new Dish("french fries", false, 530, Dish.Type.OTHER),
+	              new Dish("rice", false, 350, Dish.Type.OTHER),
+	              new Dish("season fruit", false, 120, Dish.Type.OTHER),
+	              new Dish("pizza", false, 550, Dish.Type.OTHER),
+	              new Dish("prawns", false, 300, Dish.Type.FISH),
+	              new Dish("salmon", false, 450, Dish.Type.FISH)
+	              );
+		
+		//filter and 조건 확인
+		List<String> filterMenuName = menu.stream().filter(dish -> ("pork".equals(dish.getName()) || dish.getCalories() < 400)).map(Dish::getName).collect(Collectors.toList());
+		System.out.println(filterMenuName);
+		
+		//filter not 조건 확인
+		List<String> filterMenuName2 = menu.stream().filter(dish -> !(dish.getCalories() < 400)).map(Dish::getName).collect(Collectors.toList());
+		System.out.println(filterMenuName2);
+		
+		//noneMatch 확인
+		boolean isNone = menu.stream().noneMatch(dish -> dish.getCalories() > 1000);
+		System.out.println(isNone);
+		
+		boolean isAll = menu.stream().allMatch(dish -> dish.getCalories() < 1000);
+		System.out.println(isAll);
+		
+		
+		
+		
+
+	}
+
+}
