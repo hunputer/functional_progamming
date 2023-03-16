42734c36040535374873bd6b955d1769b897947d
hunputer <wlgns3855@gmail.com>
2023-03-16 21:25:51 +0900

    commit

--- src/stream_ex7/Main.java
+++ src/stream_ex7/Main.java
@@ -2,6 +2,8 @@ package stream_ex7;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 import stream_ex7.Dish;
 
@@ -29,6 +31,18 @@ public class Main {
 		
 		long calories_Long = menu.stream().mapToLong(Dish::getCalories).sum();
 		System.out.println(calories_Long);
+		
+		//칼로리 최대값
+		OptionalInt maxCalories = menu.stream().mapToInt(Dish::getCalories).max();
+		System.out.println(maxCalories.orElse(1));
+		
+		//칼로리 최소값
+		OptionalInt minCalories = menu.stream().mapToInt(Dish::getCalories).min();
+		System.out.println(minCalories.getAsInt());
+		
+		//범위 내 짝수 카운트
+		IntStream evenNumbers = IntStream.rangeClosed(1, 100).filter(n -> n % 2 == 0);
+		System.out.println(evenNumbers.count());
 
 	}
 
