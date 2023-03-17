06c85934dac5248c4fd62a5beee9874bed052396
hunputer <wlgns3855@gmail.com>
2023-03-17 22:24:11 +0900

    commit

---
+++ src/stream_ex9/Main.java
@@ -0,0 +1,51 @@
+package stream_ex9;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import stream_ex1.Dish;
+
+public class Main {
+	public static void main(String[] args) {
+		List<Dish> menu = Arrays.asList(
+	              new Dish("pork", false, 800, Dish.Type.MEAT),
+	              new Dish("beef", false, 700, Dish.Type.MEAT),
+	              new Dish("chicken", false, 400, Dish.Type.MEAT),
+	              new Dish("french fries", false, 530, Dish.Type.OTHER),
+	              new Dish("rice", false, 350, Dish.Type.OTHER),
+	              new Dish("season fruit", false, 120, Dish.Type.OTHER),
+	              new Dish("pizza", false, 550, Dish.Type.OTHER),
+	              new Dish("prawns", false, 300, Dish.Type.FISH),
+	              new Dish("salmon", false, 450, Dish.Type.FISH)
+	              );
+		
+		//그룹핑 함수
+		//결과값 
+		Map<Dish.Type, List<Dish>> dishesByType = menu.stream().collect(Collectors.groupingBy(t -> t.getType()));
+		
+		List<Dish> others = dishesByType.get(Dish.Type.OTHER);
+		List<Dish> meat = dishesByType.get(Dish.Type.MEAT);
+		List<Dish> fish = dishesByType.get(Dish.Type.FISH);
+		
+		System.out.print("OTHERS : ");
+		for(Dish dish : others) {
+			System.out.print(dish.getName() + ", ");
+		}
+		System.out.println();
+		
+		System.out.print("MEAT : ");
+		for(Dish dish : meat) {
+			System.out.print(dish.getName() + ", ");
+		}		
+		System.out.println();
+		
+		System.out.print("FISH : ");
+		for(Dish dish : fish) {
+			System.out.print(dish.getName() + ", ");
+		}
+		System.out.println();
+	}
+
+}
